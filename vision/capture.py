6732f066b0bba7a554a40262f5319c4eee65a6a8
bb <bowenblyons@proton.me>
2026-01-05 04:16:12 -0600

    skeleton for camera class & implemented SimCamera to simulate a camera with test data

---
+++ vision/capture.py
@@ -0,0 +1,21 @@
+import cv2
+import os
+import random
+
+class Camera():
+    def capture(self):
+        raise NotImplementedError
+
+class SimCamera(Camera):
+    def __init__(self, data_dir="test_data"):
+        self.data_dir = data_dir
+        if not os.path.exists(data_dir):
+            print("Folder not found")
+    def capture(self):
+        cat = random.choice([ 'low', 'moderate', 'severe' ])
+        cat_path = os.path.join(self.data_dir, cat)
+        images = [ image for image in os.listdir(cat_path) if image.lower().endswith(('.jpg'))]
+        img = random.choice(images)
+        img_path = os.path.join(cat_path, img)
+        return cv2.imread(img_path)
+    
