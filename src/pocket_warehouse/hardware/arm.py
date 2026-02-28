d5c007b6446175e406c56d4d891ad38e7ee2d4bb
bb <bowenblyons@proton.me>
2026-02-28 06:08:45 -0600

    mvp successful run, need to calibrate movements

--- src/pocket_warehouse/hardware/arm.py
+++ src/pocket_warehouse/hardware/arm.py
@@ -6,59 +6,58 @@ from pocket_warehouse.hardware.servo import Servo
 class Arm:
     """Controls robotic arm for Hot Wheels triage system.
 
-    Manages 5 servos for base rotation, shoulder, elbow, wrist, and claw.
+    Manages 3 servos for base rotation, elbow, and claw.
     Provides high level movement operations: pickup_car, scrap, refurbish,
     resell, and review.
     """
 
     # servo indices
     SERVO_BASE: int = 0
-    SERVO_SHOULDER: int = 1
-    SERVO_ELBOW: int = 2
-    # wrist servo is bad: SERVO_WRIST: int = 3
-    SERVO_CLAW: int = 4
+    SERVO_ELBOW: int = 1
+    SERVO_CLAW: int = 2
+
+    # array for logging
+    SERVO_NAMES: list[str] = ["Base", "Elbow", "Claw"]
 
     # angles
-    HOME: int = 90
-    POSITION_PICKUP: int = 0
-    POSITION_SCRAP: int = 45
+    BASE_HOME: int = 90
+    CLAW_HOME: int = 30
+    ELBOW_HOME: int = 10
+    POSITION_PICKUP: int = 179
+    POSITION_SCRAP: int = 130
     POSITION_REFURBISH: int = 90
-    POSITION_RESELL: int = 135
-    POSITION_REVIEW: int = 180
-    ELBOW_PRELOWER: int = 40
-    SHOULDER_LOWER: int = 20
-    CLAW_OPEN: int = 140
-    CLAW_CLOSE: int = 30
+    POSITION_RESELL: int = 50
+    POSITION_REVIEW: int = 1
+    ELBOW_PARTIAL_LOWER: int = 120
+    ELBOW_LOWER: int = 140
+    CLAW_OPEN: int = CLAW_HOME
+    CLAW_CLOSE: int = 140
 
     def __init__(self):
-        self.servo_id: list[int] = [0, 1, 2, 3, 4]  # 5 is platform swivel
+        self.servo_id: list[int] = [0, 1, 2]  # 3 is platform swivel
         self.servo: Servo = Servo()
         self.__home()
 
-    def __set_angle(self, servo: int, angle: int, delay: float = 2.0) -> None:
-        """Set servo angle and wait for movement to complete."""
+    def __set_angle(self, servo: int, angle: int, delay: float = 7.5) -> None:
+        """Set servo angle and wait for movement to complete.
+
+        It's a cheap arm so patience is required...
+        """
         self.servo.set_angle(servo, angle)
+        print(f"{self.SERVO_NAMES[servo]}: set to {angle}, delay = {delay} ")
         time.sleep(delay)
 
     def __home(self) -> None:
         """Return all servos to home position"""
-        for i in self.servo_id:
-            self.__set_angle(i, self.HOME)
+        self.__set_angle(self.SERVO_BASE, self.BASE_HOME)
+        self.__set_angle(self.SERVO_CLAW, self.CLAW_HOME)
+        self.__set_angle(self.SERVO_ELBOW, self.ELBOW_HOME)
 
     def __close_claw(self) -> None:
         self.__set_angle(self.SERVO_CLAW, self.CLAW_CLOSE)
 
     def __open_claw(self) -> None:
         self.__set_angle(self.SERVO_CLAW, self.CLAW_OPEN)
-
-    def __arm_prelower(self) -> None:
-        self.__set_angle(self.SERVO_ELBOW, self.ELBOW_PRELOWER)
-
-    def __arm_lower(self) -> None:
-        self.__set_angle(self.SERVO_SHOULDER, self.SHOULDER_LOWER)
-
-    def __arm_raise(self) -> None:
-        self.__set_angle(self.SERVO_SHOULDER, self.HOME)
 
     def __position_pickup(self) -> None:
         self.__set_angle(self.SERVO_BASE, self.POSITION_PICKUP)
@@ -75,14 +74,20 @@ class Arm:
     def __position_review(self) -> None:
         self.__set_angle(self.SERVO_BASE, self.POSITION_REVIEW)
 
+    def __elbow_partial_lower(self) -> None:
+        self.__set_angle(self.SERVO_ELBOW, self.ELBOW_PARTIAL_LOWER)
+
+    def __elbow_lower(self) -> None:
+        self.__set_angle(self.SERVO_ELBOW, self.ELBOW_LOWER)
+
     def pickup_car(self) -> None:
         """Movement sequence for picking up car from platform."""
         self.__position_pickup()
-        self.__arm_prelower()
         self.__open_claw()
-        self.__arm_lower()
+        self.__elbow_partial_lower()
+        self.__elbow_lower()
         self.__close_claw()
-        self.__arm_raise()
+        self.__elbow_partial_lower()
 
     def scrap(self) -> None:
         """Movement sequence for moving car and releasing onto scrap track.
@@ -90,7 +95,7 @@ class Arm:
         Must be preceded by pickup_car()
         """
         self.__position_scrap()
-        self.__arm_lower()
+        self.__elbow_lower()
         self.__open_claw()
         self.__home()
 
@@ -100,7 +105,7 @@ class Arm:
         Must be preceded by pickup_car()
         """
         self.__position_refurbish()
-        self.__arm_lower()
+        self.__elbow_lower()
         self.__open_claw()
         self.__home()
 
@@ -110,7 +115,7 @@ class Arm:
         Must be preceded by pickup_car()
         """
         self.__position_resell()
-        self.__arm_lower()
+        self.__elbow_lower()
         self.__open_claw()
         self.__home()
 
@@ -120,6 +125,10 @@ class Arm:
         Must be preceded by pickup_car()
         """
         self.__position_review()
-        self.__arm_lower()
+        self.__elbow_lower()
         self.__open_claw()
         self.__home()
+       
+if __name__ == "__main__":
+    arm = Arm()
+    arm.pickup_car()
--- src/pocket_warehouse/main.py
+++ src/pocket_warehouse/main.py
@@ -1,6 +1,88 @@
-def main() -> None:
-    print()
+from PIL import Image
+from collections import defaultdict
 
+from pocket_warehouse.hardware.capture_images import capture_image_set
+from pocket_warehouse.inference.model import PocketWarehouseInference
+from pocket_warehouse.triage.triage import triage
+from pocket_warehouse.hardware.arm import Arm
+from pocket_warehouse.schemas.schemas import ClassificationResult, PartClassification
+from pocket_warehouse.utils.config import load_config
+
+def main() -> None:
+    # order of operations when run:
+    # 1. take pics with capture_images
+    #     - will take 3 pics
+    # 2. run inference on images
+    #     - will need to run 3 times
+    #     - will need to combine results
+    # 3. run inference through triage system
+    # 4. give appropriate commands to arm to move the car
+
+    # get set of three images of car
+    image_paths: list[str] = capture_image_set()
+
+    model = PocketWarehouseInference("models/pocket_warehouse.tflite")
+    predictions = []
+    
+    for path in image_paths:
+        image = Image.open(path)
+        predictions.append(model.predict_with_confidence(image))
+
+    combined_pred = combine_predictions(predictions)
+    print("Combined_prediction: ", combined_pred)
+    classification_result = get_classification_result(combined_pred)
+
+    cfg = load_config()
+    
+    triage_result = triage(classification_result, cfg)
+
+    print(triage_result.decision)
+
+    arm = Arm()
+    arm.pickup_car()
+
+    if (triage_result.decision == "scrap"):
+        arm.scrap()
+    elif (triage_result.decision == "refurbish"):
+        arm.refurbish()
+    elif (triage_result.decision == "resell"):
+        arm.resell()
+    else: # triage_result.decision == "review"
+        # will also trigger review for unforseen exceptions
+        arm.review()
+
+def get_classification_result(pred: dict) -> ClassificationResult:
+    axle = PartClassification("axle", pred["axle"]["severity"]["class"], pred["axle"]["severity"]["confidence"], pred["axle"]["functional"]["class"], pred["axle"]["functional"]["confidence"])
+    wheel = PartClassification("wheel", pred["wheel"]["severity"]["class"], pred["wheel"]["severity"]["confidence"], pred["wheel"]["functional"]["class"], pred["wheel"]["functional"]["confidence"])
+    frame = PartClassification("frame", pred["frame"]["severity"]["class"], pred["frame"]["severity"]["confidence"], pred["frame"]["functional"]["class"], pred["frame"]["functional"]["confidence"])
+    body = PartClassification("body", pred["body"]["severity"]["class"], pred["body"]["severity"]["confidence"], pred["body"]["functional"]["class"], pred["body"]["functional"]["confidence"])
+    paint = PartClassification("paint", pred["paint"]["severity"]["class"], pred["paint"]["severity"]["confidence"], pred["paint"]["functional"]["class"], pred["paint"]["functional"]["confidence"])
+    return ClassificationResult(axle, wheel, frame, body, paint, None)
+
+def combine_predictions(predictions: list[dict]) -> dict:
+    combined = {}
+
+    parts = predictions[0].keys()
+
+    for part in parts:
+        combined[part] = {}
+
+        for label_type in ["severity", "functional"]:
+            
+            max_conf = 0.0
+            max_conf_class = -1
+            for p in predictions:
+                pred = p[part][label_type]
+                if pred["confidence"] > max_conf:
+                    max_conf = pred["confidence"]
+                    max_conf_class = pred["class"]
+
+            combined[part][label_type] = {
+                "class": max_conf_class,
+                "confidence": max_conf
+            }
+
+    return combined
 
 if __name__ == "__main__":
     main()
